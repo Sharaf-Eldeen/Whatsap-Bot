a26895ac33ce73a99a03b8464896549d58b629c1
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-27 16:30:02 +0300

    Start The Bot with The basic logic (no session information)

---
+++ server.js
@@ -0,0 +1,20 @@
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const client = new Client();
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("message_create", (message) => {
+  if (message.body === "ping") {
+    message.reply("pong");
+  }
+});
+
+client.initialize();
