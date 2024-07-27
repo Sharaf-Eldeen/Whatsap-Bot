a8a4744f1013dc595487b6c156b319a04c130ee4
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-07-27 17:47:11 +0300

    Update The Bot To Save Session Information

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
-const client = new Client();
-
-client.on("ready", () => {
-  console.log("Client is ready!");
+const client = new Client({
+  authStrategy: new LocalAuth(),
 });
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on("message_create", (message) => {
-  if (message.body === "ping") {
-    message.reply("pong");
-  }
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
 client.initialize();
